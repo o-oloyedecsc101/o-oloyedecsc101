052cdaecc884010f9bd8d78e01c9ea563f7963e0
o-oloyedecsc101 <115807221+o-oloyedecsc101@users.noreply.github.com>
2023-02-05 16:21:33 +0100

    Add files via upload

---
+++ o.oloyede.rs
@@ -0,0 +1,116 @@
+//Database for convits 
+
+use std::io;
+fn main() {
+//input name
+println!("\ninput your name");
+let mut name = String::new();
+io::std().read_line(&mut name).expect("Not a valid string");
+println!("Name:{}",name);
+
+
+//input Ministry
+println!("\ninput your Ministry");
+let mut Ministry = String::new();
+io::std().read_line(&mut Ministry).expect("Not a valid string");
+println!("Ministry:{}",Ministry);
+
+
+//input Geopolitical zone
+println!("\ninput your Geopolitical zone");
+let mut Geopolitical_zone  = String::new();
+io::std().read_line(&mut Geopolitical_zone).expect("Not a valid string");
+println!("Geopolitical_zone:{}",Geopolitical_zone);
+
+if name = Abibogun_Alamba_Dauda && Ministry = internal_Affairs && Geopolitical_ zone = South_West{
+println!("Yes This a convict {}", Name);
+}
+else {
+println!("Not a convit");
+}
+
+if name = Murutala_AFFEEZ_BENDU && Ministry = justice && Geopolitical_ zone = North_East{
+println!("Yes This a convict {}", Name);
+}
+else {
+println!("Not a convit");
+}
+if name = Okorocha_Calistus_Ogbona && Ministry = Defence && Geopolitical_ zone = South_South{
+println!("Yes This a convict {}", Name);
+}
+else {
+println!("Not a convit");
+}
+if name = Adewale_Jimoh_Akanbi && Ministry = power & steel && Geopolitical_ zone = South_West{
+println!("Yes This a convict {}", Name);
+}
+else {
+println!("Not a convit");
+}
+if name = Osazuwa_Faith_Etieye && Ministry = petroleum && Geopolitical_ zone = South_East{
+println!("Yes This a convict {}", Name);
+}
+else {
+println!("Not a convit");
+}
+
+}
+
+fn main{
+//input YOUR PUBLIC SERVICE 
+println!("\ninput your JOB");
+let mut JOB = String::new();
+io::std().read_line(&mut JOB).expect("Not a valid string");
+println!("JOB:{}",JOB);
+
+//input YOUR YEARS OF EXPERIENCE
+println!("\nHow many years hav you worked for");
+let mut Experience = String::new();
+io::std().read_line(&mut name).expect("Not a valid string");
+let experience:i32 = experience.trim().parse().expect("Not a valid number");
+println!("Experience:{}",Experience);
+}
+
+if public service =  Office Aministator && for Experience in 1..3{
+println!("you are an intern"); 
+}
+if public service =  Office Aministator && for Experience in 3..6{
+println!("you are an Administrator"); 
+}
+if public service =  Office Aministator && for Experience in 5..9{
+println!("you are an senior Administrator"); 
+}
+if public service =  Office Aministator && for Experience in 8..11{
+println!("you are an Office Manager"); 
+}
+if public service =  Office Aministator && for Experience in 10..13{
+println!("you are an Director"); 
+}
+if public service =  Office Aministator && for Experience == 14 {
+println!("you are an CEO"); 
+}
+
+if public service =  Academic && for Experience in 1..3{
+println!("you are an --"); 
+}
+if public service =  Academic && for Experience in 3..6{
+println!("you are an Reserch Assistant"); 
+}
+if public service =  Academic && for Experience in 5..9{
+println!("you are an PhD candidate"); 
+}
+if public service =  Academic && for Experience in 8..11{
+println!("you are an Post-Doc Researcher"); 
+}
+if public service =  Academic && for Experience in 10..14{
+println!("you are an Senior Lecturer"); 
+}
+if public service =  Academic && for Experience == 14{
+println!("you are an Dean"); 
+}
+
+if public service =  Lawyer && for Experience in 1..3{
+println!("you are an paralegal"); 
+}
+
+}
